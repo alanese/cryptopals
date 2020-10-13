37ecc0b76ae65260ac5813deb9cb00ab943fc285
alanese <43454736+alanese@users.noreply.github.com>
2020-10-13 13:42:35 -0400

    challenge 26

--- main.go
+++ main.go
@@ -10,16 +10,8 @@ func main() {
 	rand.Seed(time.Now().Unix())
 	key := GenerateRandomByteSlice(16)
 
-	ecbCtext, err := DecodeFileBase64("25.txt")
-	if err != nil {
-		panic("failed decoding file")
-	}
-	ptext := DecryptAESECB(ecbCtext, []byte("YELLOW SUBMARINE"))
-	nonce := make([]byte, 8) //use eight zero bytes as the nonce
-	ptext = PKCSPad(ptext, 16)
-	cText := EncryptAESCTR(ptext, key, nonce)
-
-	decryptedText := C25BreakEdit(cText, key)
-	fmt.Println(string(decryptedText))
+	forgedToken := Challenge26ForgeData(key)
+	ok := Challenge26AdminCheck(forgedToken, key)
+	fmt.Println(ok)
 
 }
--- set_4.go
+++ set_4.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"strings"
 )
 
 //C25Edit decrypts the ciphertext (using AES-CTR with the given key),
@@ -23,4 +24,45 @@ func C25BreakEdit(ctext, key []byte) []byte {
 	keystream := C25Edit(ctext, key, zeroes, 0)
 	ptext, _ := XorBufs(ctext, keystream)
 	return ptext
+}
+
+//Challenge26Func generates, pads, and encrypts a
+//data string as per challenge 26 (C16 reimplemented with CTR)
+func Challenge26Func(userdata string, secretKey []byte) []byte {
+	front := []byte("comment1=cooking%20MCs;userdata=")
+	back := []byte(";comment2=%20like%20a%20pound%20of%20bacon")
+	userdata = strings.ReplaceAll(userdata, "=", "%3D")
+	userdata = strings.ReplaceAll(userdata, ";", "%3B")
+	ptext := append(front, []byte(userdata)...)
+	ptext = append(ptext, back...)
+	ptext = PKCSPad(ptext, 16)
+	nonce := make([]byte, 8) //use 0s as the nonce
+	return EncryptAESCTR(ptext, secretKey, nonce)
+}
+
+//Challenge26AdminCheck decrypts a byte slice
+//and checks whether it contains the text ";admin=true;"
+//(C16 reimplemented with CTR)
+func Challenge26AdminCheck(data []byte, secretkey []byte) bool {
+	nonce := make([]byte, 8) //use 0s for nonce
+	ptext := EncryptAESCTR(data, secretkey, nonce)
+	ptext, _ = StripPKCS7Padding(ptext, 16)
+	return bytes.Contains(ptext, []byte(";admin=true;"))
+}
+
+//Challenge26ForgeData creates a byte slice in the format
+//output by Challenge16Func which, when decrypted, contains
+//the text ";admin=true;" using a CTR bit-flipping attack
+//(C16 reimplemented with CTR)
+func Challenge26ForgeData(key []byte) []byte {
+	userdata := "aaaaaaaaaaaaaaaa"
+	ctext := Challenge26Func(userdata, key)
+	uBytes := []byte(userdata)
+	targetText := []byte("aaaaa;admin=true")
+	flipper, _ := XorBufs(uBytes, targetText)
+	sneakyText := ctext[:32]
+	newBlock, _ := XorBufs(flipper, ctext[32:48])
+	sneakyText = append(sneakyText, newBlock...)
+	sneakyText = append(sneakyText, ctext[48:]...)
+	return sneakyText
 }
